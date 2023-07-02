e2d31da319684f177ab3db30a95793115066c991
Wolf Utz <w.utz@omega-code.de>
2023-07-02 15:50:15 +0200

    Move some data to controller and update translations

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -19,11 +19,89 @@ class FrontendController extends AbstractController
     #[Route('/', name: 'fe_index')]
     #[Cache(maxage: 3600, public: true, mustRevalidate: true)]
     public function index(
-        Request $request,
+        Request                $request,
         EntityManagerInterface $entityManager,
-        TransportInterface $mailer,
-        TranslatorInterface $translator
-    ): Response {
+        TransportInterface     $mailer,
+        TranslatorInterface    $translator
+    ): Response
+    {
+        $services = [
+            [
+                'name' => 'page.services.shopware',
+                'image' => '/assets/img/services/shopware.png',
+                'keywords' => [
+                    'page.services.keywords.e-commerce',
+                    'page.services.keywords.cms'
+                ]
+            ],
+            [
+                'name' => 'page.services.typo3',
+                'image' => '/assets/img/services/typo3.svg',
+                'keywords' => [
+                    'page.services.keywords.cms'
+                ]
+            ],
+            [
+                'name' => 'page.services.wordPress',
+                'image' => '/assets/img/services/wordpress.svg',
+                'keywords' => [
+                    'page.services.keywords.blog',
+                    'page.services.keywords.cms',
+                ]
+            ],
+            [
+                'name' => 'page.services.symfony',
+                'image' => '/assets/img/services/symfony.svg',
+                'keywords' => [
+                    'page.services.keywords.webApp',
+                    'page.services.keywords.api',
+                    'page.services.keywords.cli',
+                ]
+            ],
+            [
+                'name' => 'page.services.spring',
+                'image' => '/assets/img/services/spring.svg',
+                'keywords' => [
+                    'page.services.keywords.webApp',
+                    'page.services.keywords.api',
+                    'page.services.keywords.cli',
+                ]
+            ],
+            [
+                'name' => 'page.services.vue',
+                'image' => '/assets/img/services/vuejs.svg',
+                'keywords' => [
+                    'page.services.keywords.webApp',
+                ]
+            ]
+        ];
+        $skills = [
+            [
+                'name' => 'page.skills.php',
+                'image' => '/assets/img/skills/php.svg',
+            ],
+            [
+                'name' => 'page.skills.java',
+                'image' => '/assets/img/skills/java.svg',
+            ],
+            [
+                'name' => 'page.skills.csharp',
+                'image' => '/assets/img/skills/csharp.svg',
+            ],
+            [
+                'name' => 'page.skills.javascript',
+                'image' => '/assets/img/skills/javascript.svg',
+            ],
+            [
+                'name' => 'page.skills.css',
+                'image' => '/assets/img/skills/css.svg',
+            ],
+            [
+                'name' => 'page.skills.html',
+                'image' => '/assets/img/skills/html.svg',
+            ],
+        ];
+
         $inquiry = new Inquiry();
         $form = $this->createForm(InquiryType::class, $inquiry);
         $form->handleRequest($request);
@@ -42,12 +120,12 @@ class FrontendController extends AbstractController
             $mailer->send($email);
 
             $this->addFlash('success', $translator->trans('form.submitSuccessful'));
-            return $this->redirect($this->generateUrl('fe_index').'#contact');
+            return $this->redirect($this->generateUrl('fe_index') . '#contact');
         }
 
-        return $this->render('index.html.twig', [
-            'form' => $form,
-        ])->setMaxAge(3600)->setPublic();
+        return $this->render('index.html.twig', ['form' => $form, 'services' => $services, 'skills' => $skills])
+            ->setMaxAge(3600)
+            ->setPublic();
     }
 
     #[Route('/impressum', name: 'fe_impressum')]
